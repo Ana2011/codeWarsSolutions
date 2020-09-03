395d8e146884b5dd2c2696957df6784b7e92b8b1
Ana <nastencheg@gmail.com>
2020-09-02 22:06:11 -0500

    Add code  was solutions Testing, Testing! Return and concatenate a string.

---
+++ Testing, Testing! Return and concatenate a string..js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/5977387e131c07082b000098/train/javascript
+
+function getRes(myScore) {
+    let result = (myScore < 70) ? 'Sorry, you have failed. Better luck next time! Thank you for taking part.' : 'Congratulations, you have passed! Thank you for taking part.';
+    return result;
+}

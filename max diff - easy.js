543d9321f00d2817d075e4941684017ae0e9d549
Ana <nastencheg@gmail.com>
2020-09-27 18:26:07 -0500

    Add code  was solutions max diff - easy

---
+++ max diff - easy.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/588a3c3ef0fbc9c8e1000095/train/javascript
+
+function maxDiff(list) {
+    if(list.length <= 1){
+        return 0;
+    }
+    let diff = 0;
+    let min = Math.min(...list);
+    let max = Math.max(...list);
+
+    diff = max - min;
+    return diff;
+};

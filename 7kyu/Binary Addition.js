2b057f219c9168315ec219696ed59f027f8087d3
Ana <nastencheg@gmail.com>
2020-11-11 22:51:57 -0600

    Solution Binary Addition

---
+++ 7kyu/Binary Addition.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/551f37452ff852b7bd000139/train/javascript
+
+function addBinary( a, b ){
+    var sum = a + b,
+        binary = '';
+
+    while ( sum > 0 ) {
+        binary = ( sum % 2 ) + binary;
+        sum = Math.floor( sum / 2 );
+    }
+
+    return binary;
+}

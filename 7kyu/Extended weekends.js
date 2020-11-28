e3ea15c082e290eba35ea45b725438ed72350a4d
Ana <nastencheg@gmail.com>
2020-11-28 16:14:55 -0600

    Solutions Extended weekends

---
+++ 7kyu/Extended weekends.js
@@ -0,0 +1,19 @@
+//https://www.codewars.com/kata/5be7f613f59e0355ee00000f/train/javascript
+
+const EXTENDED_MONTHS = [0, 2, 4, 6, 7, 9, 11];
+
+
+function solve(a, b) {
+    let range = [];
+    for (; a <= b; a++) {
+        range = range.concat(
+            EXTENDED_MONTHS.map(m => new Date(a, m, 1)).filter(d => d.getDay() === 5)
+        );
+    }
+
+    return [
+        range[0].toLocaleString("en-us", { month: "short" }),
+        range[range.length - 1].toLocaleString("en-us", { month: "short" }),
+        range.length
+    ];
+}

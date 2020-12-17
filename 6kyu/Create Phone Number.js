50193448b01f8dfa497d5682244f5cab38947b35
Ana <nastencheg@gmail.com>
2020-12-17 09:22:24 -0600

    Solutions Create Phone Number

---
+++ 6kyu/Create Phone Number.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+
+function createPhoneNumber(numbers){
+    let a = numbers.slice(0,3).join('')
+    let b = numbers.slice(3,6).join('')
+    let c = numbers.slice(6,10).join('')
+    return `(${a}) ${b}-${c}`
+}

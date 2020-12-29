76f8d718e1361c9408681b35cbb5f027aef514da
Ana <nastencheg@gmail.com>
2020-12-29 08:15:09 -0600

    Solutions makeBackronym

---
+++ 7kyu/makeBackronym.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/55805ab490c73741b7000064
+
+var makeBackronym = function(string) {
+    return string
+        .toUpperCase()
+        .split("")
+        .map(elem => dict[elem])
+        .join(" ");
+};

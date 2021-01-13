df5814c012dbf5025b58ab6f5651a35ad5cb40bf
Ana <nastencheg@gmail.com>
2021-01-13 08:10:59 -0600

    Solutions What's wrong with these identifiers?

---
+++ 8kyu/What's wrong with these identifiers.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/56bb01de0e8b29de50000b19
+
+const Person = {
+    firstname: "Ana",
+    secondname: "Gab",
+    emailAddress: "ana.gab@email.com",
+    gender: "F",
+};

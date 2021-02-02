3e86050550e909cf44e205f5f76522d2c77037f6
Ana <nastencheg@gmail.com>
2021-02-02 14:39:37 -0600

    Solutions Incorrect division method

---
+++ 8kyu/Incorrect division method.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/54d1c59aba326343c80000e7/train/javascript
+
+function solve (x,y){
+    if(y === 0){
+        return Infinity;
+    } else {
+        return x / y
+    }
+}
